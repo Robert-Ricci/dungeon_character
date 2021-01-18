460c45b6b1181e8714e9c2993227435b2f8d9eec
Robert Ricci <rmricci121@gmail.com>
2021-01-18 11:48:41 -0500

    This commit added the envirment setup to the dungeon character file

--- lib/dungeon_character.rb
+++ lib/dungeon_character.rb
@@ -1,6 +1,14 @@
-require "dungeon_character/version"
+require_relative "dungeons/version"
+require 'pry'
+require 'httparty'
+require 'colorize'
+require_relative './race'
+require_relative './klass'
+require_relative './api'
+require_relative './dungeon_controller'
 
-module DungeonCharacter
+
+module Dungeons
   class Error < StandardError; end
   # Your code goes here...
 end
